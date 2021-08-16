24008f4884bc0721657978440a23bf7b653e3426
wenjunche <wenjun@openfin.co>
2021-08-15 21:27:43 -0400

    NCBT-24: fixed test for citrix

--- citrix-csharp-demo/Program.cs
+++ citrix-csharp-demo/Program.cs
@@ -13,12 +13,20 @@ using WebSocketSharp.Server;
 
 namespace citrix_csharp_demo
 {
-    public class Echo: WebSocketBehavior
+    public class CTXService: WebSocketBehavior
     {
+        protected override void OnOpen()
+        {
+            Console.WriteLine("WS client connected");
+        }
         protected override void OnMessage(MessageEventArgs e)
         {
             Console.WriteLine("Received: {0}", e.Data);
-            Send(e.Data);
+//            Send(e.Data);
+        }
+        protected override void OnClose(CloseEventArgs e)
+        {
+            Console.WriteLine("WS Client closed: {0}", e.Reason);
         }
     }
 
@@ -28,41 +36,51 @@ namespace citrix_csharp_demo
         static extern byte VD_LoadURL(string url);
         //            [MarshalAs(UnmanagedType.LPStr)] string url);
 
+        private static String CTX_SERVICE_PATH = "/ctxservice";
 
         static void Main(string[] args)
         {
             var cmd = new RootCommand
             {
                 new Option<string>("--ws-url", "WebSocket server url"),
-                new Option<string>("--url", "Redirected URL"),
+                new Option<string>("--proxy-url", "Manifest URL for OpenFin proxy app"),
             };
 
             cmd.Handler = CommandHandler.Create<string, string, IConsole>(HandleGreeting);
             cmd.Invoke(args);
-
-
         }
 
-        static void HandleGreeting(string wsURL, string url, IConsole console)
+        static void HandleGreeting(string wsURL, string proxyUrl, IConsole console)
         {
 
-            if (url.IsNullOrEmpty())
+            if (proxyUrl.IsNullOrEmpty())
             {
-                Console.WriteLine("Missing redirect url");
+                Console.WriteLine("Missing proxy url");
             } 
             else
             {
+                string url = String.Format("{0}/?$$wsurl={1}{2}", proxyUrl, wsURL, CTX_SERVICE_PATH);
+                Console.WriteLine("Invoking proxy app at {0}", url);
                 var r = VD_LoadURL(url);
             }
 
             Console.WriteLine("Starting Websocket server a {0}", wsURL);
+            WebSocketServer wssv = new WebSocketServer(wsURL);
+            Console.WriteLine("Registring CTX service path {0}", CTX_SERVICE_PATH);
+            wssv.AddWebSocketService<CTXService>(CTX_SERVICE_PATH);
+            wssv.Start();
 
-            WebSocketServer ws = new WebSocketServer(wsURL);
-            ws.AddWebSocketService<Echo>("/echo");
+            Console.WriteLine("Enter JSON string to send to apps at client side");
+            String s;
+            do {
+                s = Console.ReadLine();
+                if (s != "exit")
+                {
+                    wssv.WebSocketServices.Hosts.FirstOrDefault().Sessions.Broadcast(s);
+                }
+            } while (s != "exit");
 
-            ws.Start();
-            Console.ReadKey();
-            ws.Stop();
+            wssv.Stop();
 
         }
 
