c853d260b2d3069184a6c21af89fc918aece3f4c
wenjunche <wenjun@openfin.co>
2021-08-18 21:03:10 -0400

    NCBT-24: added start.bat

---
+++ start.bat
@@ -0,0 +1,3 @@
+rem ws-url: URL for websocket server created by citrix-csharp-demo
+rem proxy-url: fins link for proxy OpenFin app
+citrix-csharp-demo --ws-url ws://citrix-hsot:8080 --proxy-url fin://localhost:9000/app.json
